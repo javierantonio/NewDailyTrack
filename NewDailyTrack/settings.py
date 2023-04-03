d16cd9b65ab6426997cb5dd7c094ff5c0046c195
javierantonio <48659740+javierantonio@users.noreply.github.com>
2023-04-04 07:30:40 +0800

    Working landing page

--- NewDailyTrack/settings.py
+++ NewDailyTrack/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'landingPage.apps.LandingpageConfig',
+    'registration.apps.RegistrationConfig',
+
 ]
 
 MIDDLEWARE = [
---
+++ landingPage/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='landing-page'),
+    path('registration/', include('registration.urls'), name='registration'),
+]
