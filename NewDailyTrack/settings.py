0ecacdbeaf6f7e7336ab08bcc73ac471f92f8d60
Colli039 <aspan.colline@gmail.com>
2023-08-23 08:33:04 +0800

    Changes

--- NewDailyTrack/settings.py
+++ NewDailyTrack/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dajaxice',
+    'dajax',
 
     'home.apps.LandingpageConfig',
     'registration.apps.RegistrationConfig',
